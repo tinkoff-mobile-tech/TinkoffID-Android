5722b66d5bd8692c166642698f101325f5e1b4dd
Kirill Voskrebentsev <k.voskrebentsev@tinkoff.ru>
2022-07-12 22:09:15 +0300

    MC-6146 fix tests

--- app-demo/src/androidTest/java/ru/tinkoff/core/app_demo_partner/TinkoffIdSignInButtonTest.kt
+++ app-demo/src/androidTest/java/ru/tinkoff/core/app_demo_partner/TinkoffIdSignInButtonTest.kt
@@ -33,7 +33,7 @@ class TinkoffIdSignInButtonTest : AndroidJUnitRunner() {
 
     @Test
     fun testStandardButtonTextPresence() {
-        checkPresenceTextOnScreen(originalText)
+        checkPresenceTextOnScreen(createFinalTitle(originalTitlePart))
     }
 
     @Test
@@ -42,39 +42,49 @@ class TinkoffIdSignInButtonTest : AndroidJUnitRunner() {
             standardButton.title = SOME_TEXT
         }
 
-        checkPresenceTextOnScreen(SOME_TEXT)
+        checkPresenceTextOnScreen(createFinalTitle(SOME_TEXT))
     }
 
     @Test
     fun testStandardButtonTextAbsence() {
         runOnMainThread {
-            standardButton.title = EMPTY_TEXT
+            standardButton.title = null
         }
 
-        checkAbsenceTextOnScreen(originalText)
+        checkPresenceTextOnScreen(permanentTitlePart)
     }
 
     @Test
-    fun testStandardButtonCashbackPresence() {
-        checkPresenceTextOnScreen(wrapBadge(originalCashback))
+    fun testStandardButtonBadgePresence() {
+        checkPresenceTextOnScreen(originalBadge)
     }
 
     @Test
-    fun testStandardButtonCashbackUpdate() {
+    fun testStandardButtonBadgeUpdate() {
         runOnMainThread {
             standardButton.badgeText = SOME_TEXT
         }
 
-        checkPresenceTextOnScreen(wrapBadge(SOME_TEXT))
+        checkPresenceTextOnScreen(SOME_TEXT)
     }
 
     @Test
-    fun testStandardButtonCashbackAbsence() {
+    fun testStandardButtonBadgeAbsence() {
         runOnMainThread {
-            standardButton.badgeText = EMPTY_TEXT
+            standardButton.badgeText = null
         }
 
-        checkAbsenceTextOnScreen(wrapBadge(originalCashback))
+        checkAbsenceTextOnScreen(originalBadge)
+    }
+
+    @Test
+    fun testStandardButtonAbsenceElements() {
+        runOnMainThread {
+            standardButton.isCompact = true
+        }
+
+        checkAbsenceTextOnScreen(createFinalTitle(originalTitlePart))
+        checkAbsenceTextOnScreen(originalBadge)
     }
 
     private fun checkPresenceTextOnScreen(text: String) {
@@ -83,18 +93,18 @@ class TinkoffIdSignInButtonTest : AndroidJUnitRunner() {
     private fun checkAbsenceTextOnScreen(text: String) {
         assertTrue("Found text \"$text\" on screen", !UiDevice.getInstance(getInstrumentation()).hasObject(By.textContains(text)))
     }
-    private fun wrapBadge(cashback: String) = ""
+    private fun createFinalTitle(titlePart: String) = "$titlePart $permanentTitlePart"
     private fun runOnMainThread(block: () -> Unit) {
         activityRule.runOnUiThread(block)
     }
 
     private companion object {
         private const val SOME_TEXT = "test"
-        private const val EMPTY_TEXT = ""
 
         private const val standardButtonId = R.id.standardButtonTinkoffAuth
 
-        private val originalText = getInstrumentation().targetContext.resources.getString(R.string.partner_auth_sign_in_button_title)
-        private val originalCashback = getInstrumentation().targetContext.resources.getString(R.string.partner_auth_sign_in_button_badge)
+        private val originalTitlePart = getInstrumentation().targetContext.resources.getString(R.string.partner_auth_sign_in_button_title)
+        private val permanentTitlePart = getInstrumentation().targetContext.resources.getString(ru.tinkoff.core.tinkoffId.R.string.tinkoff_id_tinkoff_text)
+        private val originalBadge = getInstrumentation().targetContext.resources.getString(R.string.partner_auth_sign_in_button_badge)
     }
 }
