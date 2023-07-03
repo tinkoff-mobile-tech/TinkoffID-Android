ce242e1a556284b1aaf204964d657c0341ef0aa3
Kirill Voskrebentsev <k.voskrebentsev@tinkoff.ru>
2023-07-03 15:56:23 +0300

    add unit tests

--- tinkoff-id/src/test/java/ru/tinkoff/core/tinkoffId/TinkoffIdAuthTest.kt
+++ tinkoff-id/src/test/java/ru/tinkoff/core/tinkoffId/TinkoffIdAuthTest.kt
@@ -49,9 +49,22 @@ public class TinkoffIdAuthTest {
         assertThat(queryParam("clientId")).isEqualTo(CLIENT_ID)
         assertThat(queryParam("code_challenge")).isNotEmpty()
         assertThat(queryParam("code_challenge_method")).isNotEmpty()
+        assertThat(queryParam("redirect_uri")).isEqualTo(REDIRECT_URL)
         assertThat(queryParam("callback_url")).isEqualTo(testUri.toString())
         assertThat(queryParam("package_name")).isNotEmpty()
         assertThat(queryParam("partner_sdk_version")).isEqualTo(BuildConfig.VERSION_NAME)
+    }
+
+    @Test
+    public fun testWebViewIntentCreation() {
+        val intent = tinkoffAuth.createTinkoffWebViewAuthIntent(testUri)
+        assertThat(intent.extras).isNotNull()
+        val queryParam = { paramName: String -> requireNotNull(intent.getStringExtra(paramName)) }
+        assertThat(queryParam("clientId")).isEqualTo(CLIENT_ID)
+        assertThat(queryParam("code_challenge")).isNotEmpty()
+        assertThat(queryParam("code_challenge_method")).isNotEmpty()
+        assertThat(queryParam("redirect_uri")).isEqualTo(REDIRECT_URL)
+        assertThat(queryParam("callback_url")).isEqualTo(testUri.toString())
     }
 
     @Test
